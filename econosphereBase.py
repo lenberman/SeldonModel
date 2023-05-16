fd34bacb2bdb88e6ad4cc6ed7680e2f938f59612
Len Berman <lenberman@gmail.com>
2023-05-15 20:39:16 -0400

    Improved printing

--- econosphereBase.py
+++ econosphereBase.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 import pdb; 
 now = 0
+import pdb; pdb.set_trace()
 class Region:
     ## regions is a dict with indexed by index-tuples.
     def __init__(self, regions, size=1):
@@ -38,12 +39,16 @@ class Region:
 class Event:  # space time chunk starting now
     events = {}
     def __init__(self, t=now, i=None, j=None, k=None):
-        val = (t, i, j, k)
+        self.time = t
+        self.space = (i,j,k)
         try:
-            val = Event.events[val]
+            return Event.events[(self.time,self.space)]
         except KeyError:
-            Event.events[val] = val
-        self.val = val
+            Event.events[(self.time,self.space)] = (self.time,self.space)
+
+    def __str__(self):
+        return "(@" + str(self.time) + "T, loc:" + str(self.space) + ")"
+        
 
 class UseValue:  #fear power friendship love medium-of-exchange
     uvId = 0
@@ -71,22 +76,37 @@ class UseValue:  #fear power friendship love medium-of-exchange
         return self.name + "(" + str(self.uvId) + ")"
         
 #  directed  edges with (multi-dimsensional) capacity and adjustable delay.
-class edg:
+class Edge:
     edgeDict = {}
+
+    @classmethod
+    def connect(cls, src, tgt):
+        return cls(src, tgt)
+
+    def __str__(self):
+            return str(vars(self.edge[0])) + "\n\t>>\n" + str(vars(self.edge[1])) + "@(" + str(self.start) + ", " + str(self.end) + ")\n"
+
     # create edge from (src->tgt || tgt->src)
     def __init__(self, src=None , tgt=None, forward=True, end=None, start = now):
         self.forward = forward
         if forward == True:
-            self.edge = [src, tgt]
+            self.edge = (src, tgt)
         else:
-            self.edge = [tgt, src]
+            self.edge = (tgt, src)
         self.start = start
         self.end = end
-                 
 
+        def reverse(self):
+            self.edge = (self.edge[1], self.edge[0])
+
+##Node
 class Node:   # # Node in Seldon decomposition
     indx = 0
     nodes = {}
+
+    def __rshift__(self, tgt):
+        edge = Edge.connect(self, tgt)
+        return edge
 
     def __init__(self, name, information, event=Event()):  # # any Node may have lifetime
         # increment then stor
@@ -103,10 +123,20 @@ class Node:   # # Node in Seldon decomposition
         self.edges.append(tmp)
         return tmp
             
-            
+    def __str__(self):
+        rv = str(vars(self)) + str(self.birth)
+        return rv
 
     def where(self, when=now):...
         
     def step(self, until=now+1):
         ...
 
+
+if __name__ == '__main__':    
+    nd = Node("test", None)
+    nd1 = Node("test1", None)
+    edge = nd >> nd1
+    pprint(str(edge))
+    pprint(str(nd))
+    
